f587c0a89e146ce584dd82811489e467119a6e6f
Pheerawat Nanna <bomozios.jp@gmail.com>
2022-02-07 16:29:36 +0700

    import camera libs

---
+++ galahad/src/main/java/com/apthai/apcameraxcore/galahad/ApCameraActivity.kt
@@ -0,0 +1,11 @@
+package com.apthai.apcameraxcore.galahad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ap_camera)
+    }
+}
