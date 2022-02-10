8613f1fb4e7dcee435c73c06e5e4e3533a32a703
Pheerawat Nanna <bomozios.jp@gmail.com>
2022-02-10 11:09:17 +0700

    get camera on land

--- galahad/src/main/java/com/apthai/apcameraxcore/galahad/ApCameraActivity.kt
+++ galahad/src/main/java/com/apthai/apcameraxcore/galahad/ApCameraActivity.kt
@@ -6,6 +6,9 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
@@ -46,6 +49,17 @@ class ApCameraActivity : ApCameraBaseActivity<ApCameraViewModel>(), ApCameraNavi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        @Suppress("Deprecation")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
         activityApCameraBinding = ActivityApCameraBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
