08d68b6eb46dac7ade8f13efaf01db31fe2065b9
Pheerawat Nanna <bomozios.jp@gmail.com>
2022-02-14 15:21:48 +0700

    fix warning from ktlint

--- common/src/main/java/com/apthai/apcameraxcore/common/ApCameraBaseActivity.kt
+++ common/src/main/java/com/apthai/apcameraxcore/common/ApCameraBaseActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class ApCameraBaseActivity<V : ApCameraBaseViewModel<*>> : AppCompatActivity() {
 
-    abstract fun tag() : String
+    abstract fun tag(): String
     abstract fun setUpView()
     abstract fun initial()
 }
