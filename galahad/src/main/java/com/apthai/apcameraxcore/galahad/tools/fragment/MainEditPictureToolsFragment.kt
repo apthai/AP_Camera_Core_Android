2cfb65cbecb907f78fa2e60d7a2e5659988b9644
Pheerawat Nanna <bomozios.jp@gmail.com>
2022-03-22 12:12:25 +0700

    initial button

--- galahad/src/main/java/com/apthai/apcameraxcore/galahad/tools/fragment/MainEditPictureToolsFragment.kt
+++ galahad/src/main/java/com/apthai/apcameraxcore/galahad/tools/fragment/MainEditPictureToolsFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.apthai.apcameraxcore.common.ApCameraBaseFragment
+import com.apthai.apcameraxcore.galahad.R
 import com.apthai.apcameraxcore.galahad.databinding.FragmentMainEditPictureToolsBinding
 import com.bumptech.glide.Glide
 
 class MainEditPictureToolsFragment : ApCameraBaseFragment<MainEditPictureToolsViewModel>(),
-    MainEditPictureToolsNavigator {
+    MainEditPictureToolsNavigator, View.OnClickListener {
 
     companion object {
         const val TAG_FRAGMENT_NAME = "MainEditPictureToolsFragment"
@@ -49,19 +51,32 @@ class MainEditPictureToolsFragment : ApCameraBaseFragment<MainEditPictureToolsVi
     }
 
     override fun setUpView() {
-        this.viewBinding.frmMainEditPictureToolsIcCrop.setOnClickListener {
-
-        }
-        this.viewBinding.frmMainEditPictureToolsIcWrite.setOnClickListener {
-
-        }
 
         Glide.with(this).load(this.getPathFileFromArg())
             .into(this.viewBinding.frmMainEditPictureToolsImageView)
     }
 
     override fun initial() {
+        this.viewBinding.frmMainEditPictureToolsIcBack.setOnClickListener(this)
+        this.viewBinding.frmMainEditPictureToolsIcCrop.setOnClickListener(this)
+        this.viewBinding.frmMainEditPictureToolsIcWrite.setOnClickListener(this)
+    }
 
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.frm_main_edit_picture_tools_ic_back ->{
+                activity?.finish()
+            }
+            R.id.frm_main_edit_picture_tools_ic_crop ->{
+                Toast.makeText(activity, "Crop is coming soon", Toast.LENGTH_SHORT).show()
+            }
+            R.id.frm_main_edit_picture_tools_ic_write ->{
+                Toast.makeText(activity, "Write or draw is coming soon", Toast.LENGTH_SHORT).show()
+            }
+            else ->{
+
+            }
+        }
     }
 
     override fun getPathFileFromArg(): String {
