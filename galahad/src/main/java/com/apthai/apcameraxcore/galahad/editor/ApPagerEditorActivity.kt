8ffd4cd47427d3be8d4f91d96a0c196fdda341f0
Pheerawat Nanna <bomozios.jp@gmail.com>
2022-04-18 17:27:39 +0700

    [wip]

--- galahad/src/main/java/com/apthai/apcameraxcore/galahad/editor/ApPagerEditorActivity.kt
+++ galahad/src/main/java/com/apthai/apcameraxcore/galahad/editor/ApPagerEditorActivity.kt
@@ -1,14 +1,26 @@
 package com.apthai.apcameraxcore.galahad.editor
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.apthai.apcameraxcore.common.ApCameraBaseActivity
+import com.apthai.apcameraxcore.common.model.ApPhoto
 import com.apthai.apcameraxcore.galahad.databinding.ActivityGalahadPagerEditorBinding
 import com.apthai.apcameraxcore.galahad.editor.adapter.PagerEditorAdapter
 
 class ApPagerEditorActivity : ApCameraBaseActivity<ApPagerEditorViewModel>(), ApPagerEditorNavigator {
 
     override fun tag(): String = ApPagerEditorActivity::class.java.simpleName
+
+    companion object{
+
+        private const val AP_EDITOR_PHOTOS_LIST_PAYLOAD = "ap_editor_photos_list_payload"
+
+        fun getInstance(context : Context, apPhotoList : MutableList<ApPhoto>) : Intent = Intent(context, ApPagerEditorActivity::class.java).apply {
+            putParcelableArrayListExtra(AP_EDITOR_PHOTOS_LIST_PAYLOAD, ArrayList(apPhotoList))
+        }
+    }
 
     private var activityGalahadPagerEditorBinding : ActivityGalahadPagerEditorBinding?=null
     private val binding get() = activityGalahadPagerEditorBinding
@@ -33,4 +45,7 @@ class ApPagerEditorActivity : ApCameraBaseActivity<ApPagerEditorViewModel>(), Ap
     override fun setUpView() {}
 
     override fun initial() {}
+
+    override fun getApPhotoListPayload(): MutableList<ApPhoto> = intent.getParcelableArrayListExtra<ApPhoto>(
+        AP_EDITOR_PHOTOS_LIST_PAYLOAD)?.toMutableList() ?: ArrayList()
 }
--- galahad/src/main/java/com/apthai/apcameraxcore/galahad/editor/ApPagerEditorNavigator.kt
+++ galahad/src/main/java/com/apthai/apcameraxcore/galahad/editor/ApPagerEditorNavigator.kt
@@ -1,4 +1,8 @@
 package com.apthai.apcameraxcore.galahad.editor
 
+import com.apthai.apcameraxcore.common.model.ApPhoto
+
 interface ApPagerEditorNavigator {
+
+    fun getApPhotoListPayload() : MutableList<ApPhoto>
 }
