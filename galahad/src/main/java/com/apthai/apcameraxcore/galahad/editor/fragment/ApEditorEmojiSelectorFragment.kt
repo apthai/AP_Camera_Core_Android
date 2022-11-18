748f60cb8635763053e7bbf79fbfb1ca245954df
ธีรนัย อสิพงษ์ <theeranai_a@apthai.com>
2022-11-18 15:01:33 +0700

    check point fixed emoji list

--- galahad/src/main/java/com/apthai/apcameraxcore/galahad/editor/fragment/ApEditorEmojiSelectorFragment.kt
+++ galahad/src/main/java/com/apthai/apcameraxcore/galahad/editor/fragment/ApEditorEmojiSelectorFragment.kt
@@ -2,14 +2,14 @@ package com.apthai.apcameraxcore.galahad.editor.fragment
 
 import android.annotation.SuppressLint
 import android.app.Dialog
-import android.content.Context
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.apthai.apcameraxcore.galahad.R
 import com.apthai.apcameraxcore.galahad.databinding.FragmentApEditorStickerEmojiDialogBinding
-import com.apthai.apcameraxcore.galahad.editor.PhotoApp.Companion.photoApp
 import com.apthai.apcameraxcore.galahad.editor.fragment.adapter.EmojiListAdapter
+import com.apthai.apcameraxcore.galahad.util.EmojiConst
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -34,6 +34,7 @@ class ApEditorEmojiSelectorFragment :
     private var emojiListAdapter: EmojiListAdapter? = null
     private var listener: OnEmojiSelectedListener? = null
 
+    @Suppress("DEPRECATION")
     @SuppressLint("RestrictedApi")
     override fun setupDialog(dialog: Dialog, style: Int) {
         super.setupDialog(dialog, style)
@@ -47,7 +48,12 @@ class ApEditorEmojiSelectorFragment :
             if (behavior != null && behavior is BottomSheetBehavior<*>) {
                 behavior.setBottomSheetCallback(mBottomSheetBehaviorCallback)
             }
-            (ctView.parent as View).setBackgroundColor(resources.getColor(android.R.color.transparent))
+            (ctView.parent as View).setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    android.R.color.transparent
+                )
+            )
             val gridLayoutManager = GridLayoutManager(activity, 5)
             binding?.apEditorEmojiStickerRecyclerView?.layoutManager = gridLayoutManager
             emojiListAdapter = EmojiListAdapter(requireActivity(), emojisList)
@@ -73,20 +79,19 @@ class ApEditorEmojiSelectorFragment :
     }
 
     companion object {
-        private var emojisList = getEmojis(photoApp)
+        private var emojisList = getEmojis()
 
         /**
          * Provide the list of emoji in form of unicode string
-         *
-         * @param context context
          * @return list of emoji unicode
          */
-        private fun getEmojis(context: Context?): ArrayList<String> {
+        private fun getEmojis(): ArrayList<String> {
             val convertedEmojiList = ArrayList<String>()
-            val emojiList = context?.resources?.getStringArray(R.array.photo_editor_emoji)
-            emojiList?.let { emojiUnicodeList ->
+            EmojiConst.EMOJI_LIST.toMutableList().let { emojiUnicodeList ->
                 for (emojiUnicode in emojiUnicodeList) {
-                    convertedEmojiList.add(convertEmoji(emojiUnicode))
+                    val emoji = convertEmoji(emojiUnicode)
+                    if (emoji.isNotEmpty())
+                        convertedEmojiList.add(emoji)
                 }
             }
             return convertedEmojiList
